678b18b249fb69947d2b8f86d48476fd2045862c
Lester <lestersl@685b35c89b4a.ant.amazon.com>
2015-04-23 19:49:49 -0700

    updates

--- src/main/java/com/hackathon/PersistenceConfig.java
+++ src/main/java/com/hackathon/PersistenceConfig.java
@@ -16,13 +16,18 @@ public class PersistenceConfig {
 	@Bean
 	BasicDataSource dataSource() throws URISyntaxException {
 
-		String dbString = "jdbc:mysql://hackdb.c4edy7boveqz.us-west-2.rds.amazonaws.com:3306";
+		String dbString = "jdbc:mysql://hackdb.c4edy7boveqz.us-west-2.rds.amazonaws.com:3306/productdb";
 
 		BasicDataSource result = new BasicDataSource();
 		result.setUrl(dbString);
 		result.setUsername("admin");
 		result.setPassword("password");
-		result.setDriverClassName("org.mysql.Driver");
+		result.setDriverClassName("com.mysql.jdbc.Driver");
+		
+		result.setMaxActive(10);
+		
+		System.out.println("this worked");
+		
 		return result;
 	}
 
@@ -32,12 +37,9 @@ public class PersistenceConfig {
 		entityManagerFactory.setDataSource(dataSource());
 		entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 		Properties jpaProperties = new Properties();
-		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		jpaProperties.put("hibernate.show_sql", "true");
 		jpaProperties.put("hibernate.hbm2ddl.auto", "update");
-		jpaProperties.put("hibernate.cache.use_query_cache", "true");
-		jpaProperties.put("hibernate.cache.use_second_level_cache", "true");
-		jpaProperties.put("hibernate.cache.region.factory_class","org.hibernate.cache.ehcache.EhCacheRegionFactory");
 		entityManagerFactory.setJpaProperties(jpaProperties);
 		return entityManagerFactory;
 	}
--- src/main/java/com/hackathon/WebMVCConfig.java
+++ src/main/java/com/hackathon/WebMVCConfig.java
@@ -1,20 +1,9 @@
 package com.hackathon;
 
-import java.net.URISyntaxException;
-import java.util.Properties;
-
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
