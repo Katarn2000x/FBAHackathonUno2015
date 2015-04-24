142174ba92f2b0d158bae1637926f475e69a65cb
Arthur Jones <jonearth@amazon.com>
2015-04-23 19:04:14 -0700

    http requests

--- src/main/java/com/hackathon/controllers/ProductController.java
+++ src/main/java/com/hackathon/controllers/ProductController.java
@@ -1,21 +1,28 @@
 package com.hackathon.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hackathon.dao.ProductDao;
-import com.hackathon.dao.ProductDaoImpl;
 
 @RestController
 public class ProductController {
 	
     @Autowired ProductDao productDao;
     
-	@RequestMapping("/product")
-	public String getProduct(@RequestParam(value="name", defaultValue="Ryan") String name) {
-	    
-		return productDao.getProduct("test").getTitle();
-	}
+    @RequestMapping("/products/random")
+    public String getProduct() {
+        
+        return productDao.getProduct("test").getTitle();
+    }
+    
+    @RequestMapping("/products/query/{query}")
+    public String getProduct(@PathVariable("query") String query) {
+        
+        
+        
+        return productDao.getProduct("test").getTitle();
+    }
  }
